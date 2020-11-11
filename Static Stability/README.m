ffa4eef2cdc54507c617f45b08f64ee5d1986dd6
ts7618 <ts7618@ic.ac.uk>
2020-11-11 22:11:41 +0000

    Pulled wing parameters from wingDesign.mat

--- Static Stability/README.m
+++ Static Stability/README.m
@@ -1,10 +1,18 @@
 clear
 clc
 
-addpath('../Initial Sizing/', '../Xfoil/', '../Wing Design')
-load('InitialSizing.mat', 'W0', 'WingLoading')
+addpath('../Initial Sizing/', '../Xfoil/', '../Aerodynamics')
+load('InitialSizing.mat', 'W0', 'WingLoading', 'AspectRatio')
+load('wingDesign.mat', 'MAC', 'b', 'Sweep_quarterchord', 'TaperRatio')
+
 
 SWing = W0/WingLoading; %wing reference area based on initial sizing
+ARwing = AspectRatio;
+cBarWing = MAC;
+wingSpan = b;
+quarterSweepWing = Sweep_quarterchord;
+taperWing = TaperRatio;
+clear AspectRatio MAC b Sweep_quarterchord TaperRatio
 
 %horizontal and vertical stabiliser 
 etaH = 0.9; %crude approximation for jet planes
@@ -17,14 +25,14 @@ ARvert = 2; %typically 1.3-2 where AR = h^2/Sv and h is the tail height
 
 [SHoriz, SVert, spanHoriz, heightVert] = tailplaneSizing(cBarWing, wingSpan, SWing, xWing, xHoriz, xVert, VbarH, VbarV, ARhoriz, ARvert);
 
-CMalphaF = fuselagePitchingMoment(lf, wf, cBarWing, SWing, xWing);
-
-downwash = downwash(lHoriz, hHoriz, wingSpan, quarterSweepWing, ARwing, taperWing, CLalphaW, CLalphaW_M0);
-
-%power-off neutral point and static margin
-xNPOff = neutralPoint(SWing, SHoriz, xWing, cBarW, CLalphaH, CLalphaW, CMalphaF, VbarH, downwash, etaH);
-KnOff = (xNPOff - xCG)/cBarW;
-
-%power-on static margin and neutral point
-KnOn = KnOff - 0.02;
-xNPPOn = KnOn*cBarW + xCG;
+% CMalphaF = fuselagePitchingMoment(lf, wf, cBarWing, SWing, xWing);
+% 
+% downwash = downwash(lHoriz, hHoriz, wingSpan, quarterSweepWing, ARwing, taperWing, CLalphaW, CLalphaW_M0);
+% 
+% %power-off neutral point and static margin
+% xNPOff = neutralPoint(SWing, SHoriz, xWing, cBarWing, CLalphaH, CLalphaW, CMalphaF, VbarH, downwash, etaH);
+% KnOff = (xNPOff - xCG)/cBarWing;
+% 
+% %power-on static margin and neutral point
+% KnOn = KnOff - 0.02;
+% xNPPOn = KnOn*cBarW + xCG;
