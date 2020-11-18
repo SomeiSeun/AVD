e7609c9bc916381351272bb5a863e923a7e41319
Tanvi <trg18@ic.ac.uk>
2020-11-18 18:06:18 +0000

    Changed drag due to fuselage eq

--- Aerodynamics/MiscD.m
+++ Aerodynamics/MiscD.m
@@ -10,6 +10,6 @@ function [C_Duc,C_Dflaps,C_Dwe,C_Dfu,CD_Misc]=MiscD(Area_ucfrontal,Sref,flapspan
 C_Duc=2.25*(Area_ucfrontal/Sref);   % same value for take-off and landing. Nothing for cruise
 C_Dflaps=0.0023*(flapspan/wingspan)*flapdeflection; %flaps and slats are not deployed during cruise. Only take-off and values will be calculated.
 C_Dwe=0.3*(A_eff/Sref); % Same value for take-off,cruise and landing. Will be calculated for no enginer failure, one engine failure and all engine failure
-C_Dfu=3.83*((pi*d^2)/4*Sref)*beta^2.5; % one value for all three segments
+C_Dfu=3.83*((pi*d^2)/(4*Sref))*(beta)^2.5 % one value for all three segments
 CD_Misc=C_Duc+C_Dflaps+C_Dwe+C_Dfu;
 end 
