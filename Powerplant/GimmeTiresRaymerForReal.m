5f75aacebccd1bbc3798b06590d7dfc8df112a2f
Palash Mattoo <palashmattoo@gmail.com>
2020-11-19 22:47:12 +0000

    Undercarriage
    Almost done, can now use as a function in optimiser

--- Powerplant/GimmeTiresRaymerForReal.m
+++ Powerplant/GimmeTiresRaymerForReal.m
@@ -1,22 +1,13 @@
-function[BestWheelType, BestWheelReferenceNumber, BestWheelRatedInflationPSI] = GimmeTiresRaymerForReal(LoadReq, OptimiseFor)
+function[Wheel] = GimmeTiresRaymerForReal(LoadReq, OptimiseFor)
 % Example: [BestWheelType, BestWheelReferenceNumber,
 % BestWheelRatedInflationPSI] = GimmeTiresRaymerForReal(LoadReq,
 % OptimiseFor) where:
     % LoadReq is the load required for that single wheel
     % OptimiseFor = 1 to find wheel with smallest diameter
     % OptimiseFor = 2 to find wheel with smallest width
+    % OptimiseFor = 3 to find wheel with smallest PSI
 % This function would sort tires that work for requested load and find the
-% one with the smallest diameter. Can upgrade the code to find one with the
-% smallest width instead using a simple boolean functionality.
-
-%% Housekeeping (remove at the end)
-clear
-clc
-close all
-
-%% Hard coded for now
-LoadReq = 45000;
-OptimiseFor = 1; % 1 = Minimum diameter, 2 = minimum width
+% one with the smallest value for requested parameter.
 
 %% Load entire wheel database
 load('Wheels_All.mat')
@@ -29,16 +20,38 @@ if OptimiseFor == 1
     MinDiam = min(InflatedOuterDiamMaxINCH(Index1));
     Index2 = find(InflatedOuterDiamMaxINCH(Index1) == MinDiam);
     A = Index1(Index2);
-    BestWheelReferenceNumber = ReferenceNumber(A);
-    BestWheelRatedInflationPSI = RatedInflationPSI(A); 
-    BestWheelType = TypeOfTire(A);
-elseif OptimiseFor ==2
+elseif OptimiseFor == 2
     MinWidth = min(InflatedSectionWidthMaxINCH(Index1));
     Index2 = find(InflatedSectionWidthMaxINCH(Index1) == MinWidth);
     A = Index1(Index2);
-    BestWheelReferenceNumber = ReferenceNumber(A);
-    BestWheelRatedInflationPSI = RatedInflationPSI(A);  
-    BestWheelType = TypeOfTire(A);
+elseif OptimiseFor == 3
+    MinPSI = min(RatedInflationPSI(Index1));
+    Index2 = find(RatedInflationPSI(Index1) == MinPSI);
+    A = Index1(Index2);
 end
+A = A(1);
+Wheel.Index = A;
+Wheel.Type = TypeOfTire(A);
+Wheel.ReferenceNumber = ReferenceNumber(A);
+Wheel.TTorTL = TTorTL(A);
+Wheel.RatedSpeedMPH = RatedSpeedMPH(A);
+Wheel.RatedLoadLBS = RatedLoadLBS(A);
+Wheel.RatedInflationPSI = RatedInflationPSI(A);
+Wheel.MaxBrakingLoadLBS = MaxBrakingLoadLBS(A);
+Wheel.MaxBottomingLoadLBS = MaxBottomingLoadLBS(A);
+Wheel.OuterDiamMaxINCH = InflatedOuterDiamMaxINCH(A);
+Wheel.OuterDiamMinINCH = InflatedOuterDiamMinINCH(A);
+Wheel.SectionWidthMaxINCH = InflatedSectionWidthMaxINCH(A);
+Wheel.SectionWidthMinINCH = InflatedSectionWidthMinINCH(A);
+Wheel.ShoulderDiamMaxINCH = ShoulderDiamMaxINCH(A);
+Wheel.ShoulderWidthMaxINCH = ShoulderWidthMaxINCH(A);
+Wheel.StaticLoadedRadiusINCH = StaticLoadedRadiusINCH(A);
+Wheel.FlatTireRadiusINCH = FlatTireRadiusINCH(A);
+Wheel.AspectRatio = TireAspectRatio(A);
+Wheel.RimSize = WheelRimSize(A);
+Wheel.WidthBetweenFlangesINCH = WheelWidthBetweenFlangesINCH(A);
+Wheel.SpecifiedRimDiamINCH = WheelSpecifiedRimDiamINCH(A);
+Wheel.FlangeHeightINCH = WheelFlangeHeightINCH(A);
+Wheel.MinLedgeWidthINCH = WheelMinLedgeWidthINCH(A);
 
 end
--- Powerplant/UndercarriageNew.m
+++ Powerplant/UndercarriageNew.m
@@ -1,4 +1,5 @@
-%function[] = Undercarriage(W0, x_cg_max, x_cg_min, y_cg, z_cg_max, z_cg_min, Length_ac, x_ng_min, x_fuse_tapers)
+%function[] = Undercarriage(W0, WF6, x_cg_max, x_cg_min, y_cg, z_cg_max, z_cg_min, ...
+    Length_ac, x_ng_min, x_fuse_tapers, AoA_GroundMax, groundclearance, y_mg_max)
 
 %% Undercarriage
 
@@ -6,47 +7,49 @@
 % for sizing the undercarriage.
 
 %% Housekeeping
-clear
-clc
-close all
+%clear
+%clc
+%close all
 
 %% Get data
-load('../Initial Sizing/InitialSizing.mat', 'W0')
+%load('../Initial Sizing/InitialSizing.mat', 'W0', 'WF6')
 disp(['The mass of aircraft is ', num2str(round(W0*2.20462262/9.81)), ' lbs'])
 
 %% Step 1: Assume tire pressure, number of u/c struts and wheels per strut
 
 % Decisions
-NoseTirePressure = 150;
-MainTirePressure = 150;
-NoseStruts = 1;
-NoseWheelsPerStrut = 2;
-MainStruts = 2;
-MainWheelsPerStrut = 4;
+% NoseTirePressure = 150;
+% MainTirePressure = 150;
+% NoseStruts = 1;
+% NoseWheelsPerStrut = 2;
+% MainStruts = 2;
+% MainWheelsPerStrut = 4;
 
 %% Step 2: Place u/c (assuming CG height and position)
 
 % Inputs 1: Assuming CG height and position
-x_cg_max = 32;                                                                  % Allowing for the fact that CG approximations are not going to be accurate yet
-x_cg_min = 30;                                                                  % See above
+
+%x_cg_max = 32;                                                                  % Allowing for the fact that CG approximations are not going to be accurate yet
+%x_cg_min = 30;                                                                  % See above
 x_cg_avg = (x_cg_max + x_cg_min)/2;                                             % If required
-y_cg = 0;                                                                       % For completeness sake, should be zero for symmetric aircraft
-z_cg_max = 3;                                                                   % Again allowing deviation
-z_cg_min = 2;                                                                   % Value not really used tbh so might not be required
+%y_cg = 0;                                                                       % For completeness sake, should be zero for symmetric aircraft
+%z_cg_max = 3;                                                                   % Again allowing deviation
+%z_cg_min = 2;                                                                   % Value not really used tbh so might not be required
 z_cg_avg = (z_cg_max + z_cg_min)/2;                                             % If required
 
 % Inputs 2: Assuming other inputs required to place u/c
-Length_ac = 60;                                                                 % Only really relevant for plotting the graph within limits
+
+%Length_ac = 60;                                                                 % Only really relevant for plotting the graph within limits
 W_NoseGear_Ratio_Max = 0.20;                                                    % Assumption from slides - to prevent overloading
 W_NoseGear_Ratio_Min = 0.05;                                                    % Assumption from slides - to prevent loss of steering friction
-x_ng_min = 5;                                                                   % Foremost position of nose gear possible (based on nose and cockpit geometry)
-x_fuse_tapers = 50;                                                             % Point where fuselage starts to taper upwards (for tailstrike calculations)
-AoA_liftoff = 5;                                                                % Required to know largest angle encountered near ground (for tailstrike calculations)
-AoA_landing = 2;                                                                % Angles given in degrees
-AoA_GroundMax = max([AoA_landing, AoA_liftoff]);                                % Simplify
-ground_clearance = 1.5;                                                         % Required for a lot of things, probably would need to be iterated within the code
+%x_ng_min = 5;                                                                   % Foremost position of nose gear possible (based on nose and cockpit geometry)
+%x_fuse_tapers = 50;                                                             % Point where fuselage starts to taper upwards (for tailstrike calculations)
+%AoA_liftoff = 5;                                                                % Required to know largest angle encountered near ground (for tailstrike calculations)
+%AoA_landing = 2;                                                                % Angles given in degrees
+%AoA_GroundMax = max([AoA_landing, AoA_liftoff]);                                % Simplify
+%ground_clearance = 1.5;                                                         % Required for a lot of things, probably would need to be iterated within the code
 overturn_angle = 63;                                                            % Assumption from slides
-y_mg_max = 3;                                                                   % Limitations can arise due to structural constraints
+%y_mg_max = 3;                                                                   % Limitations can arise due to structural constraints
 
 % Functions
 [x_NoseGear, x_MainGear] = PlaceMyUndercarriage(x_cg_max, x_cg_min, ...
@@ -74,7 +77,8 @@ disp(['Static load on main gear is approx ', num2str(round(LbsReqMainGear)), ' l
 
 %% Step 4: Choose tires
 
-[WheelType, WheelReferenceNumber, RatedInflationPSI] = GimmeTiresRaymerForReal(LbsReqMainGear, 1); % For main gear
+[NoseWheel] = GimmeTiresRaymerForReal(LbsReqNoseGearStatic + LbsReqNoseGearDynamic, 1);
+[MainWheel] = GimmeTiresRaymerForReal(LbsReqMainGear, 1);
 
 %% Step 5: Estimate ACN
 
@@ -92,26 +96,36 @@ disp(['Static load on main gear is approx ', num2str(round(LbsReqMainGear)), ' l
 % landing gear without needing to perform this ACN PCN runway suitability
 % check.
 
-%% Step 6: Size oleos and find strut length
+%% Step 6: Verify you can operate from desired airfields with chosen u/c
 
+% Ignore for now
 
+%% Step 7: Size oleos and find strut length
 
+% The undercarriage would be designed assuming a telescopic configuration
+% for both nose and main gears. This is to minimise the length of the oleos
+% which should help with smaller retraction bay sizes. This also means that
+% the wheel stroke is the same as the strut stroke.
 
+% Inputs
+V_Vertical = 10; %ft/s
+eta_oleo = 0.9; %nondim
+eta_tire = 0.47; %nondim
+N_g = 2.7; %taken as lower bound to give largest stroke
+Stroke_tire_main = ((MainWheel.OuterDiamMinINCH/2) - MainWheel.StaticLoadedRadiusINCH)/12; %ft
+Stroke_tire_nose = ((NoseWheel.OuterDiamMinINCH/2) - NoseWheel.StaticLoadedRadiusINCH)/12; %ft
 
+% Functions
+MainOleo = GimmeAnOleo(V_Vertical, eta_oleo, eta_tire, N_g, Stroke_tire_main, W0*WF6, 2); %ft
+NoseOleo = GimmeAnOleo(V_Vertical, eta_oleo, eta_tire, N_g, Stroke_tire_nose, W0*WF6, 1); %ft
 
+% Outputs
+disp(['The height of undercarriage is approx ', num2str( (MainOleo.TotalLength + MainWheel.OuterDiamMinINCH/2) * 0.0254 ), ' m'])
 
+%% Step 8: Repeat for actual CG range
 
-
-
-
-
-
+% CG range is already taken into account. Extreme load cases already
+% considered in the constraint diagram.
 
 
 %end
-
-
-
-
-
-
