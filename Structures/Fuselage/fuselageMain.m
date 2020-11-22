4b306498820bc86f8933981e40275a52402dfaf0
Tanmay <tanmay.ubgade18@imperial.ac.uk>
2020-11-22 17:20:59 +0100

    fixed random error
    random erorr?

--- Structures/Fuselage/fuselageMain.m
+++ Structures/Fuselage/fuselageMain.m
@@ -15,7 +15,7 @@ fusDiamInner = fusDiamOuter-wallThickness;
 
 % Front
 frontFR = 1.4;
-<<<<<<< HEAD
+
 frontLength = 5.839;
 %{
 noseLength = 1.35;
@@ -30,7 +30,7 @@ frontLength2 = noseLength + cockpitLength + entranceLength;
 %}
 cockpitArea = 48.777;
 noseArea = 5.046;
-=======
+
 frontLength = 1.4*fusDiamOuter;
 
 noseLength = 1.35;
@@ -42,17 +42,14 @@ cockpitArea = 48.777;
 entranceLength = 1.92;
 
 %frontLength2 = noseLength + cockpitLength + entranceLength;
->>>>>>> main
+
 frontArea = noseArea + cockpitArea;
 
 
 
 % Main
-<<<<<<< HEAD
+
 mainLength = 36.538; 
-=======
-mainLength = 35.538; 
->>>>>>> main
 mainArea = 479.325;
 
 % Aft
@@ -62,10 +59,7 @@ aftArea = 0.589 + 81.371;
 
 % Total
 totalLength = frontLength + aftLength + mainLength;
-<<<<<<< HEAD
 totalArea = frontArea + mainArea + aftArea;
-=======
-totalArea = noseArea + mainArea + aftArea;
->>>>>>> main
+
 
 save fuselageOutputs
