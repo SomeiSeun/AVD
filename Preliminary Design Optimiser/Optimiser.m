153ce5dbb8840a99fd8cb3d58f24384bc6c9c114
Tanvi <trg18@ic.ac.uk>
2020-11-25 17:34:04 +0000

    HLD lift analysis added

--- Preliminary Design Optimiser/Optimiser.m
+++ Preliminary Design Optimiser/Optimiser.m
@@ -16,24 +16,27 @@ close all
 %load('../Initial Sizing/InitialSizing.mat')
 load('../Initial Sizing/InitialSizing.mat')
 load('../Aerodynamics/wingDesign.mat')
-load('../Static Stability/tailplane_Sizing_Variable_Values.mat')
+load('tailplane_Sizing_Variable_Values.mat')
 load('../Aerodynamics/Unknowns.mat')                                        % This is just a bunch of values I am still waiting on...
-
+load('../Structures/Fuselage/fuselageOutputs.mat')      
 
 %% Assumed values for now
 
 %For aerodynamics analysis
 l=[MAC,fuselage_length,nacelle_length,H_MAC,V_MAC];
-xtocmax=[xtocmax,0,0,0.3,0.3];
+xtocmax=[xtocmax_wing,0,0,0.3,0.3];
 ttoc=[Airfoil_ThicknessRatio_used,0,0,0.12,0.12];
 theta_max=[Sweep_maxt,0,0,27,27];
 S_wet_all=[S_wetted,606,60,0,0];
-
+flapped_ratio=0.487;
+chord_ratio=1.26;
+Sweep_TE=20.9647;
 
 %% Optimiser loop
 % Aerodynamic Lift- lift curve slope and clmax for clean config determined. HLD effects remain
 % but as values are updated/changed, outputs below should change :)
-[CL_a,CL_max_clean,CL_aflaps,delta_alpha_zero,delta_CL_max]=WingLift(AspectRatio,S_exposed,Sref,d,b,M,Sweep_maxt,Cl_am,chord_ratio,Cl_wing_airfoil,Sflapped,Sweep_quarterchord,sweep_HLD,delta_alpha_zeroairfoil);
+                                                                    
+[CL_a,CL_max_clean,CL_aflaps,delta_alpha_takeoff,delta_alpha_landing,delta_CL_max,CL_max_takeoff,CL_max_landing]=WingLift(AspectRatio,S_exposed,Sref,d,b,M,Sweep_maxt,Cl_am,chord_ratio,Cl_wing_airfoil,flapped_ratio,Sweep_quarterchord,Sweep_TE);
 %Aerodynamic Drag (Cruise Conditions)
 [C_Duc_cruise,C_Dflaps_cruise,C_Dwe_cruise,C_Dfu_cruise]=MiscD(Area_ucfrontal,Sref,flapspan,b,flapdeflection,Aeff,d,upsweep_angle);
 [CD_Parasitic,CD_Parasitic_Total,CD_LandP,Re,Cfc]=Parasitic(rho_cruise,V_Cruise,l,nu_cruise,M_Cruise,xtocmax,ttoc,theta_max,fuselage_length,fuselage_diameter,nacelle_length,nacelle_diameter,S_wet_all,Sref);
