eee403de84db4ac42ee9353345274a0e3ab17e76
Tanvi <trg18@ic.ac.uk>
2020-12-02 19:31:21 +0000

    AerodynamicInputs

--- Aerodynamics/TailLift.m
+++ Aerodynamics/TailLift.m
@@ -1,6 +1,6 @@
-function [CL_a,CL_max_clean]=TailLift(AR_HT,d,b,M,sweepanglemax,Cl_am,Cl_max,sweep_quarterchord)
+function [CL_a,CL_max_clean]=TailLift(AR_HT,fusDiamOuter,b,M,sweepanglemax,Cl_am,Cl_max,sweep_quarterchord)
 beta=sqrt(1-(M).^2);% to account for compressibility effects;
-F=1.07*(1+(d/b))^2;
+F=1.07*(1+(fusDiamOuter/b))^2;
 % fuselage spillover lift factor
 etah=0.95; %airfoil efficiency factor
 a=2*pi*AR_HT*1;
