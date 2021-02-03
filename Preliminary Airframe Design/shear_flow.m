187a2909e2dfd0a5d5fadbe2e070e1781f9d151a
Uddeshya <usaini2000@gmail.com>
2021-02-03 23:12:55 +0000

    added graphs to the function

--- Preliminary Airframe Design/shear_flow.m
+++ Preliminary Airframe Design/shear_flow.m
@@ -1,4 +1,4 @@
-function [tw1,tw2] = shear_flow(n, K_s, rho, V, E, b1, b2, flex_ax, cm0, cg)
+function [tw1,tw2,h] = shear_flow(n, K_s, rho, V, E, b1, b2, flex_ax, cm0, cg)
 
 % This function is used to find the thicknesses for the spar web
 
@@ -20,11 +20,10 @@ function [tw1,tw2] = shear_flow(n, K_s, rho, V, E, b1, b2, flex_ax, cm0, cg)
 
 %{
 TO DO LIST:
-1. Need pitching moment coefficient for NACA 64-215
-2. Need the CG value for NACA 64-215
-3. Need to confirm distance a in Torque calculations
-4. Need the spar height (Make this an input)(a more accurate value from Thiago)
-5. Need to take into account the torque due to the engine
+1. Need the CG value for NACA 64-215
+2. Need to confirm distance a in Torque calculations
+3. Need the spar height (Make this an input)(a more accurate value from Thiago)
+4. Need to take into account the torque due to the engine
 %}
 
 % Loading in the .mat file to get the required variables
@@ -38,38 +37,66 @@ q0 = zeros(1,length(wing.span));
 
 % This loop is used to find multiple variables
 for i = 1:length(wing.span)
-    h = 0.12 * wing.chord(i);                                                    % Approx height of the spars
-    pitchingmoment = 0.5 * rho * V^2 * wing.chord(i)^2 * cm0;                    % Pitching moment for this aerofoil
+    h(i) = 0.12 * wing.chord(i);                                                    % Approx height of the spars
+    pitchingmoment(i) = 0.5 * rho * V^2 * wing.chord(i)^2 * cm0;                    % Pitching moment for this aerofoil
     wing.torque(i) = (wing.lift(i) * abs(flex_ax - b1) * wing.chord(i)) +...
-        (n * wing.selfWeight(i) * abs(flex_ax - cg))- pitchingmoment;            % Torque distribution along the wing
-    wing.sparwebarea(i) = abs(b1 - b2) * wing.chord(i) * h;                      % Area of spar web approximately
-    q1(i) = wing.shearForce(i) / (2 * h);                                        % q1 shear flow component
+        (n * wing.selfWeight(i) * abs(flex_ax - cg))- pitchingmoment(i);            % Torque distribution along the wing
+    wing.sparwebarea(i) = abs(b1 - b2) * wing.chord(i) * h(i);                      % Area of spar web approximately
+    q1(i) = -wing.shearForce(i) / (2 * h(i));                                        % q1 shear flow component
     q0(i) = wing.torque(i) / (2 * wing.sparwebarea(i));                          % q0 shear flow component
-    qweb1 = q1(i) + q0(i);                                                       % Front spar shear flow
-    qweb2 = q1(i) - q0(i);                                                       % Rear spar shear flow
-    x1 = (qweb1 * h^2)/ (K_s * E);                                               % Value to be cube rooted for front spar
-    x2 = (qweb2 * h^2)/ (K_s * E);                                               % Value to be cube rooted for rear spar
+    qweb1(i) = abs(q1(i) + q0(i));                                                       % Front spar shear flow
+    qweb2(i) = abs(q1(i) - q0(i));                                                       % Rear spar shear flow
+    x1 = (qweb1(i) * h(i)^2)/ (K_s * E);                                               % Value to be cube rooted for front spar
+    x2 = (qweb2(i) * h(i)^2)/ (K_s * E);                                               % Value to be cube rooted for rear spar
     tw1(i) = nthroot(x1,3);                                                      % Thickness for front spar
     tw2(i) = nthroot(x2,3);                                                      % Thickness for rear spar
 end
 
+% Converting the thicknesses to mm
+tw1 = tw1*1000;
+tw2 = tw2*1000;
+
 % Plotting the torque distribution
 figure 
-plot(wing.span,wing.torque,'-xr')
+plot(wing.span,wing.torque,'.r')
+grid on
 xlabel('Wing span (m)')
 ylabel('Torque (Nm)')
 
 % Plotting wingbox area variation
 figure
-plot(wing.span,wing.sparwebarea,'-xb')
+plot(wing.span,wing.sparwebarea,'.b')
+grid on
 xlabel('Wing span (m)')
 ylabel('Area (m^2)')
 
-% Plotting shear flow term variation
+% Plotting shear flow component variation
 figure
-plot(wing.span,q1,'-xr')
+plot(wing.span,q1,'.r')
 hold on
-plot(wing.span,q0,'-xb')
+plot(wing.span,q0,'.b')
+grid on
 xlabel('Wing span (m)')
 ylabel('Shear flow (N/m)')
-legend({'q1','q0'},'Location','northwest')
+legend({'q1','q0'},'Location','northeast')
+
+% Plotting the thickness variations
+figure
+plot(wing.span,tw1,'.r')
+hold on
+plot(wing.span,tw2,'.b')
+grid on
+xlabel('Wing span (m)')
+ylabel('Thickness (mm)')
+legend({'Front spar','Rear spar'},'Location','northeast')
+
+% Plotting the front and rear spar shear flow
+figure
+plot(wing.span,qweb1,'.r')
+hold on
+plot(wing.span,qweb2,'.b')
+grid on
+xlabel('Wing span (m)')
+ylabel('Shear flow (N/m)')
+legend({'Front spar','Rear spar'},'Location','northeast')
+end
