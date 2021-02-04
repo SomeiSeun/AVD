32ed684ccf0e4a31c2946eea9ca063a7e9ec5eb1
ts7618 <ts7618@ic.ac.uk>
2021-02-04 17:10:56 +0000

    started work on sparSizing.m function

--- Preliminary Airframe Design/sparSizing.m
+++ Preliminary Airframe Design/sparSizing.m
@@ -1,4 +1,26 @@
-clear
-close all
-clc
+function [frontSpar, rearSpar] = sparSizing(NACA, wing, SparMaterial, frontSparLocation, rearSparLocation,...
+    neutralAxisLocation, frontSparHeight, rearSparHeight)
 
+
+% loading material parameters and wing momment distribution
+numSections = length(wing.span);
+E = [SparMaterial.YM]';
+
+% loading NACA aerofoil for visualisation
+fileID = fopen(NACA, 'r');
+NACA = fscanf(fileID, '%f %f', [2 Inf]);
+
+% normalised chord-wise locations of front and rear spars
+
+
+frontSpar.height = frontSparHeight*wing.chord;
+rearSpar.height = rearSparHeight*wing.chord;
+
+fig1 = figure(1);
+hold on
+plot(NACA(1,1:26), NACA(2,1:26), 'b')
+plot(NACA(1,27:end), NACA(2,27:end), 'b')
+plot([frontSparLocation frontSparLocation], [neutralAxisLocation + frontSparHeight/2, neutralAxisLocation - frontSparHeight/2])
+plot([rearSparLocation rearSparLocation], [neutralAxisLocation + rearSparHeight/2, neutralAxisLocation - rearSparHeight/2])
+axis equal
+grid minor
