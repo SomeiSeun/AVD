d159bf96a5f1aaaffc7cbfc6e4ea756ddfe7fd02
Anudi <anudi.bandara18@imperial.ac.uk>
2021-02-08 19:24:21 +0400

    more changes

--- Preliminary Airframe Design/skinStringerPanel.m
+++ Preliminary Airframe Design/skinStringerPanel.m
@@ -20,12 +20,12 @@ FS_pos=0.25;
 RS_pos=0.75;
 
 %skin-stringer panel calculations
-b2=0.12; %thickness of the aerofoil used as height of wing box
+b2=0.12*wing.chord; %thickness of the aerofoil used as height of wing box
 
 %length of wing box at each station 
 c_alongSpan=(RS_pos-FS_pos)*wing.chord; %length of wing box
-N_alongSpan=wing.bendingMoment./(c_alongSpan*b2); %bending moment per unit length at each station
-t2_alongSpan= ((((N_alongSpan*(b2^2))./(3.62*YM_of_Materials(1))).^(1/3)))*1000;
+N_alongSpan=wing.bendingMoment./(c_alongSpan.*b2); %bending moment per unit length at each station
+t2_alongSpan= ((((N_alongSpan.*(c_alongSpan.^2))./(3.62*YM_of_Materials(1))).^(1/3)))*1000;
 
 %enforce a minimum thickness of 1mm
 for i=1:numSections
