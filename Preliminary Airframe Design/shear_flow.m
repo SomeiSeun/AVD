272ee1004647c19df059523a904f834e220e9ae6
ts7618 <ts7618@ic.ac.uk>
2021-02-08 13:44:56 +0000

    sparSizing.m working but values are not realistic...

--- Preliminary Airframe Design/shear_flow.m
+++ Preliminary Airframe Design/shear_flow.m
@@ -42,10 +42,10 @@ for i = 1:length(wing.span)
     pitchingmoment(i) = 0.5 * rho * V^2 * wing.chord(i)^2 * cm0;                    % Pitching moment for this aerofoil
     wing.torque(i) = (wing.lift(i) * (flex_ax - 0.25) * wing.chord(i)) +...
         (wing.selfWeight(i) * (cg - flex_ax)) * wing.chord(i) - pitchingmoment(i);  % Torque distribution along the wing
-    wing.sparwebarea(i) = abs(b1 - b2) * wing.chord(i) * 0.5 *...
+    wing.boxArea(i) = abs(b1 - b2) * wing.chord(i) * 0.5 *...
         (frontsparweb.h(i) + rearsparweb.h(i));                                     % Approx area of central wing box
     q1(i) = -wing.shearForce(i) / (2 * frontsparweb.h(i));                          % q1 shear flow component
-    q0(i) = wing.torque(i) / (2 * wing.sparwebarea(i));                             % q0 shear flow component
+    q0(i) = wing.torque(i) / (2 * wing.boxArea(i));                             % q0 shear flow component
     frontsparweb.qweb(i) = abs(q1(i) + q0(i));                                      % Front spar shear flow
     rearsparweb.qweb(i) = abs(q1(i) - q0(i));                                       % Rear spar shear flow
     x1 = (frontsparweb.qweb(i) * frontsparweb.h(i)^2)/ (K_s * E);                   % Value to be cube rooted for front spar
@@ -62,62 +62,62 @@ end
 % rearsparweb.tw1 = rearsparweb.tw1 * 1000;
 
 % Plotting the torque distribution
-figure 
-plot(wing.span,wing.torque,'.r')
-grid on
-xlabel('Wing span (m)')
-ylabel('Torque (Nm)')
-title('Torque Spanwise Distribution')
-
+% figure 
+% plot(wing.span,wing.torque,'.r')
+% grid on
+% xlabel('Wing span (m)')
+% ylabel('Torque (Nm)')
+% title('Torque Spanwise Distribution')
+% 
 % Plotting wingbox area variation
-figure
-plot(wing.span,wing.sparwebarea,'.b')
-grid on
-xlabel('Wing span (m)')
-ylabel('Area (m^2)')
-title('Wingbox area Spanwise Distribution')
-
+% figure
+% plot(wing.span,wing.sparwebarea,'.b')
+% grid on
+% xlabel('Wing span (m)')
+% ylabel('Area (m^2)')
+% title('Wingbox area Spanwise Distribution')
+% 
 % Plotting shear flow component variation
-figure
-plot(wing.span,q1,'.r')
-hold on
-plot(wing.span,q0,'.b')
-grid on
-xlabel('Wing span (m)')
-ylabel('Shear flow (N/m)')
-legend({'q1','q0'},'Location','Northeast')
-title('Shear flow component spanwise distribution')
-
+% figure
+% plot(wing.span,q1,'.r')
+% hold on
+% plot(wing.span,q0,'.b')
+% grid on
+% xlabel('Wing span (m)')
+% ylabel('Shear flow (N/m)')
+% legend({'q1','q0'},'Location','Northeast')
+% title('Shear flow component spanwise distribution')
+% 
 % Plotting the thickness variations
-figure
-plot(wing.span,frontsparweb.tw,'.r')
-hold on
-plot(wing.span,rearsparweb.tw,'.b')
-grid on
-xlabel('Wing span (m)')
-ylabel('Thickness (mm)')
-legend({'Front spar','Rear spar'},'Location','Northeast')
-title('Thickness of spars spanwise distribution')
-
+% figure
+% plot(wing.span,1000*frontsparweb.tw,'.r')
+% hold on
+% plot(wing.span,1000*rearsparweb.tw,'.b')
+% grid on
+% xlabel('Wing span (m)')
+% ylabel('Thickness (mm)')
+% legend({'Front spar','Rear spar'},'Location','Northeast')
+% title('Thickness of spars spanwise distribution')
+% 
 % Plotting the front and rear spar shear flow
-figure
-plot(wing.span,frontsparweb.qweb,'.r')
-hold on
-plot(wing.span,rearsparweb.qweb,'.b')
-grid on
-xlabel('Wing span (m)')
-ylabel('Shear flow (N/m)')
-legend({'Front spar','Rear spar'},'Location','Northeast')
-title('Front and rear spar shear flow spanwise distribution')
-
+% figure
+% plot(wing.span,frontsparweb.qweb,'.r')
+% hold on
+% plot(wing.span,rearsparweb.qweb,'.b')
+% grid on
+% xlabel('Wing span (m)')
+% ylabel('Shear flow (N/m)')
+% legend({'Front spar','Rear spar'},'Location','Northeast')
+% title('Front and rear spar shear flow spanwise distribution')
+% 
 % Plotting front and rear spar shear stress
-figure
-plot(wing.span,frontsparweb.shearstress,'.r')
-hold on
-plot(wing.span,rearsparweb.shearstress,'.b')
-grid on
-xlabel('Wing span (m)')
-ylabel('Shear stress (N/m^2)')
-legend({'Front spar','Rear spar'},'Location','Northeast')
-title('Front and rear spar shear stress spanwise distribution')
+% figure
+% plot(wing.span,frontsparweb.shearstress,'.r')
+% hold on
+% plot(wing.span,rearsparweb.shearstress,'.b')
+% grid on
+% xlabel('Wing span (m)')
+% ylabel('Shear stress (N/m^2)')
+% legend({'Front spar','Rear spar'},'Location','Northeast')
+% title('Front and rear spar shear stress spanwise distribution')
 end
--- Preliminary Airframe Design/sparSizing.m
+++ Preliminary Airframe Design/sparSizing.m
@@ -1,39 +1,49 @@
-function [spar, Ixx, Area] = sparSizing(wing, SparMaterial, spar)
+function [spar] = sparSizing(wing, SparMaterial, spar)
 
-n = 200;
+n = 5e2;
 numSections = length(wing.span);
 
 % Determining target Ixx values for front and rear spars for each spar
 % material TYS available
 spar.Ixx = wing.bendingMoment.*spar.h./(2*SparMaterial.TYS);
 
-% Defining range of values for flange thickness tf and flange breadth b (m)
-tf = linspace(0.001, 0.1, n)';
-b = linspace(0.001, 0.5, n);
+spar.curvature = zeros(1,numSections);
+spar.displacement = zeros(1,numSections);
+for i = 2:numSections
+    spar.curvature(i) = spar.curvature(i-1) + trapz(wing.span(i-1:i), 2*SparMaterial.TYS./(SparMaterial.YM*spar.h(i-1:i)));
+    spar.displacement(i) = spar.displacement(i-1) + trapz(wing.span(i-1:i), spar.curvature(i-1:i));
+end
+spar.curvature = spar.curvature - spar.curvature
 
+% Defining range of values for flange thickness tf and flange breadth b (m)
+tf = linspace(1e-3, 0.5, n)';
+b = linspace(1e-3, 0.5, n);
+
+spar.tf = zeros(1, numSections);
+spar.b = zeros(1, numSections);
+spar.Area = zeros(1, numSections);
 % Initialising arrays
-Ixx = zeros(n, n, numSections);
-Area = zeros(n, n, numSections);
 for i = 1:numSections
-    Ixx(:,:,i) = 1/6*b.*tf.^3 + 1/2*b.*tf.*(spar.h(i) - tf).^2 + 1/12*spar.tw(i).*(spar.h(i) - 2*tf).^3;
-    Area(:,:,i) = 2*b.*tf + spar.tw(i).*(spar.h(i) - 2*tf);  
+    clc
+    disp(['Progress: ' num2str(i) '/' num2str(numSections)])
+
+    Ixx = 1/6*b.*tf.^3 + 1/2*b.*tf.*(spar.h(i) - tf).^2 + 1/12*spar.tw(i).*(spar.h(i) - 2*tf).^3;
+    Area = 2*b.*tf + spar.tw(i).*(spar.h(i) - 2*tf);  
     
     % Checking if calculated Ixx values meet minimum required Ixx
-    for j = 1:n
-        for k = 1:n
-            if Ixx(j,k,i) < spar.Ixx(i)
-                Ixx(j,k,i) = NaN;
-                Area(j,k,i) = NaN;
-            end
-        end
-    end
+    Ixx(Ixx < spar.Ixx(i)) = NaN;
+    Area(isnan(Ixx)) = NaN;
     
-    [~,I] = min(Area(:,:,i),[],[1 2], 'linear', 'omitnan');
-    [I1(i), I2(i)] = ind2sub(size(Area(:,:,i)), I);
+    % Finding minimum combination of b and tf that meets Ixx requirement
+    [~,I] = min(Area,[],[1 2], 'linear');
+    [I1, I2] = ind2sub(size(Area), I);
     
-    spar.tf(i) = tf(I1(i));
-    spar.b(i) = b(I2(i));
+    % Preparing function output
+    spar.tf(i) = tf(I1);
+    spar.b(i) = b(I2);
+    spar.Area(i) = Area(I1,I2);
 end
 
+spar.mass = -1000*SparMaterial.Density*trapz(wing.span, spar.Area);
 
 end
