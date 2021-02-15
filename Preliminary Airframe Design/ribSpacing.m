f994e90f4be3d6ea2439c5832e892840d4e39cf3
Tanvi <trg18@ic.ac.uk>
2021-02-15 21:45:47 +0000

    Rib Spacing optimisation WORKS :)
    Function added to wingStructures.m and plots added

--- Preliminary Airframe Design/ribSpacing.m
+++ Preliminary Airframe Design/ribSpacing.m
@@ -1,39 +1,41 @@
 %% Rib Sizing, Spacing and Optimisation
 
-% function[IxxPanel,fCrush,ribThickness]=ribSpacing(skinThicknessDist,wing,Optimum,c_alongSpan,N_alongSpan)
-% Calculate Second Moment of Area of Skin Panel (all values in metres)
-E=71.8e9;
-F=0.95;
+function[rSpacing,optRibSpacing,massWingBox,massEffRib,massEffSS]=ribSpacing(wing,Optimum,boxHeight,skinThicknessDist,N_alongSpan)
+%Inputs to add to function later...
 densitySS=2.7;
 densityRibs=2.7;
+F=0.95;
+E=71.8e9;
 boxLength=wing.boxLength;
-boxArea=wing.boxArea;
+% Calculate effective skin thickness and second moment of Area of panel
 T=skinThicknessDist+(Optimum.aStringer/(Optimum.numberStringer*Optimum.stringerPitch));
-IxxPanel=(boxLength.*((T.^3)/12) + T.*(0.5*boxHeight).^2);
+IxxPanel=(boxLength.*(T.^3)/12) +(boxLength.* T.*(0.5*boxHeight).^2);
 
-% Find Optimum Rib Spacing
-rSpacing=0.3:0.001:1.0; 
+%% Determining Optimum Rib Spacing 
+discSpan=wing.span(1)/1000; % Discretise wing into sections
+rSpacing=0.1:0.001:1.3; % Array of rib spacings
+
+% Loop runs through a range of rib spacings and calculates the total mass
+% of wing box
 for i=1:length(rSpacing)
-        fCrush(i,:)=(wing.bendingMoment.*rSpacing(i).*(boxHeight).*(T).*boxLength)/(2*E*(IxxPanel).^2); 
-        ribThickness(i,:)=(fCrush(i,:).*((boxHeight.*(wing.bendingMoment).^2)).^2)/(3.62.*boxLength*E);
-        
-        % Apply BC to ensure rib thickness is greater than 1mm 
-        BC1=ribThickness<0.001; 
+        fCrush(i,:)=((wing.bendingMoment.^2)*(rSpacing(i)).*(boxHeight).*(T).*boxLength)/(2*E*(IxxPanel.^2)); 
+        num=(fCrush(i,:).*(boxHeight.^2));
+        den=3.62*E*boxLength;
+        ribThickness(i,:)=(num/den).^1/3;
+
+        % Apply boundary conditions to ensure rib thickness is greater than 1mm
+        BC1=ribThickness<0.001;
         ribThickness(BC1)=0.001;
         
        %% To Optimise, consider achieving optimum mass: 
-          effectiveSST(i,:)=1/F*sqrt(rSpacing(i)*N_alongSpan/E);                                % Effective Thickness calculated from mean stress realised by the skin and stringer at failure.
-          effectiveRT(i,:)=(boxHeight.*ribThickness(i,:))/rSpacing(i);
-
+        effectiveSST(i,:)=(1/F)*(rSpacing(i)*(N_alongSpan/E)).^(1/2);                           % Effective Thickness calculated from mean stress realised by the skin and stringer at failure.
+        effectiveRT(i,:)=(boxHeight.*ribThickness(i,:))/(rSpacing(i));
+        massEffSS(i,:)=sum((densitySS*effectiveSST(i,:).*boxLength.*discSpan),2);
+        massEffRib(i,:)=sum((densityRibs*effectiveRT(i,:).*boxLength.*discSpan),2);
 end
 massWingBox=massEffRib + massEffSS;
-[optMass,minindex]=min(massWingBox); 
- optRibSpacing=rSpacing(minindex);
- 
-% end
-
-
-%% Plots of Results
-
+[~,minMassIndex]=min(massWingBox); 
+optRibSpacing=rSpacing(minMassIndex);
+end
 
 
