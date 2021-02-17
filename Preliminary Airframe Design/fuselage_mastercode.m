eb055419dba2deabc5ee36eb816dbae2a5e504a9
Uddeshya <usaini2000@gmail.com>
2021-02-17 12:57:13 +0000

    Added pressurisation to the main fuselage code

--- Preliminary Airframe Design/fuselage_mastercode.m
+++ Preliminary Airframe Design/fuselage_mastercode.m
@@ -4,73 +4,83 @@ clear
 clc
 close all
 
-load('ConceptualDesign.mat', 'W0',  'components', 'rho_cruise', 'V_Cruise', 'mainLength')
+load('ConceptualDesign.mat', 'W0', 'components', 'rho_landing', 'V_landing', 'mainLength', 'fusDiamOuter')
 %load('Materials.mat', 'SparMaterial', 'UpperSkinMaterial')
 %^ Using same materials for fuselage as the wings?
 
-%% Anudi -- this is not the proper code, simply a breakdown of the procedure. Code in progress
-%materials (sheet 1 of spreadsheet)
+D = fusDiamOuter;
 
-%fuselage materials not selected yet so using temporary values for now (values in sample spreadsheet)
-E = 73000;      % N/mm^2
+%% Anudi -- this is not the proper code, simply a breakdown of the procedure. Code in progress
+% Materials (sheet 1 of spreadsheet)
+
+% Fuselage materials not selected yet so using temporary values for now (values in sample spreadsheet)
+E = 73000;                   % N/mm^2
 Poisson = 0.33;
 G = E/2/(1+Poisson);
 Bulk_Mod = E/3/(1-2*Poisson);
-TensileYieldStress = 324;   % MPa
+TensileYieldStress = 324;    % MPa
 ShearYieldStress = TensileYieldStress/sqrt(3);
 
-%% fuselage inertial load distributions (sheet 2 of spreadsheet)
-%discretize fuselage - already done in fuselage.length from concep design
-numSections = 12;
-Nz = 1.5*2.5; % load case
+%% Fuselage inertial load distributions (sheet 2 of spreadsheet)
+% Discretize fuselage - already done in fuselage.length from concep design
+numSections = 12;  % MIGHT BE BETTER TO HAVE MORE DISCRETISATIONS? 12 SEEMS TOO LITTLE (US)
+Nz = 1.5*2.5;      % Load case
 
-%allocate weights of each component to discretized points along span of fuselage
+% Allocate weights of each component to discretized points along span of fuselage
 
-%use force and moment eqm to get RS and FS reactions
+% Use force and moment eqm to get RS and FS reactions
 
-%plot inertial load distributions
+% Plot inertial load distributions
 
-%plot shear force distribution - must be 0 at the end (Stringers needs to
-%be done before this step)
+% Plot shear force distribution - must be 0 at the end (Stringers needs to
+% be done before this step)
 fuselage = shear_flow_fuselage(A_s, y_s, Sy, I_xx, A_fus, r, b, N);
 
-%plot BM distribution
+% Plot BM distribution
 fuselage = fuselage_distributions(components, Nz, numSections, W0, mainLength);
 
-%% fuselage tail load distribution
+%% Fuselage tail load distribution
 % DON'T THINK THAT WE NEED THIS SINCE WE ONLY HAVE 1 LOADING CASE TO WORRY
 % ABOUT? (US)
 
+%% Landing load case 
 
+% Use values from notes as a starting point, as per the videos
+StringerSpacing=convlength(7,'in','m');  % Range is 6.5-9 inches
 
-%% landing load case 
-
-%use values from notes as a starting point, as per the videos
-StringerSpacing=convlength(7,'in','m'); %range is 6.5-9 inches
-%StringerShape: Z stringers
-FrameDepth=convlength(4.0, 'in','m'); %range is 3.5-4.4
+% StringerShape: Z stringers
+FrameDepth=convlength(4.0, 'in','m');    % Range is 3.5-4.4
 FrameSpacing=convlength(20, 'in','m');
-%materials to use: 2000 series for skin and 7000 series for stringer
 
-%assume constant stringer pitch for both tensile side and compression side
+% Materials to use: 2000 series for skin and 7000 series for stringer
+
+% Aassume constant stringer pitch for both tensile side and compression side
 
 
-%% presurisation 
-% ratio of cylindrical fus thickness to hemispherical ends thickness
+%% Presurisation 
+% Ratio of cylindrical fus thickness to hemispherical ends thickness
 t_c = ((2 - poisson) / (1 - poisson)) * t_s;    % t_c is cylindrical fus thickness
 
-%% bending in fuselage
-%use a datasheet to choose stringer area, as there are too many unknowns
-%use a brute force method, select As and ts and then iterate twice. Three
-%variables would be As, ts and pitch b. Choose the configuration that gives
-%lowest weight.
+% Finding out the thicknesses required to survive pressurisation
+% P is pressure 
+% d is fuselage diameter
+fuselage.thickness_h = (P * D / (2 * TensileYieldStress));  % Thickness due to hoop stress
+fuselage.thickness_l = (P * D / (4 * TensileYieldStress));  % Thickness due to longitudinal stress
+% ^ The thickness value which is larger is chosen and added to the skin
+% thickness 
 
-% we don't have to do the design for every discretised station- simply look
-% for worst case and use that to select variables. then use those same
+%% Bending in fuselage
+% Use a datasheet to choose stringer area, as there are too many unknowns
+% Use a brute force method, select As and ts and then iterate twice. Three
+% variables would be As, ts and pitch b. Choose the configuration that gives
+% lowest weight.
+
+% We don't have to do the design for every discretised station- simply look
+% for worst case and use that to select variables. Then use those same
 % variables everywhere
 
 
-%% light frames
+%% Light frames
 fuselage = light_frames(E, D, M, L);
 
 % Plotting a 3D graph for thickness variation against flange width and web
@@ -93,13 +103,13 @@ zlabel('Frame area (m^2)')
 title('Frame area surface plot')
 colorbar
 
-%% heavy frames
+%% Heavy frames
 [fuselage,theta_deg] = wise_curves(P, R, T, Q);
 
 % Plotting the Wise curves
 figure
 plot(theta_deg,fuselage.tangent_m,'-r')
-xlabel('Angle (Radians)')
+xlabel('Angle (Degrees)')
 ylabel('Bending moment (Nm)')
 title('Bending moment variation around the fuselage ring')
 
@@ -107,9 +117,9 @@ figure
 plot(theta_deg,fuselage.tangent_n,'-r')
 hold on
 plot(theta_deg,fuselage.tangent_s,'-b')
-xlabel('Angle (Radians)')
+xlabel('Angle (Degrees)')
 ylabel('Force (N)')
 title('Shear and normal force variation around the fuselage ring')
 legend({'Normal','Shear'},'Location','North')
 
-%reaction shear flow around ring equation
+% Rreaction shear flow around ring equation
